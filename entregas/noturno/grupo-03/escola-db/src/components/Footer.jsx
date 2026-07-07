5499d125c4f660084b7e0f0e332b2775e97079a2
letxiz <letxizalves@gmail.com>
2026-07-07 11:23:49 -0400

    feat: iniciando o projeto e adiciona o frontend

---
+++ entregas/noturno/grupo-03/escola-db/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        © 2026 Escola DB — Banco de Dados e SQL | Grupo 03 | Turma Noturno
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ entregas/noturno/grupo-03/escola-db/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Escola DB</h1>
+
+      <p>
+        Sistema de Gerenciamento de Alunos • Banco de Dados e SQL • Grupo 03
+      </p>
+    </header>
+  );
+}
+
+export default Header;
