47d5b0794595dec71ed168d3ffecf319a6d27a7a
WelderBm <welderb40@gmail.com>
2026-07-06 21:09:52 -0400

    feat: implement data fetching for students and classes in App component

--- entregas/vespertino/grupo-03/app/front/src/App.jsx
+++ entregas/vespertino/grupo-03/app/front/src/App.jsx
@@ -15,15 +15,13 @@ function App() {
   const [turmaSelecionada, setTurmaSelecionada] = useState("");
 
   useEffect(() => {
-    // fetch("http://localhost:5000/api/alunos")
-    //   .then((res) => res.json())
-    //   .then(setAlunos);
+    fetch("http://localhost:5000/api/alunos")
+      .then((res) => res.json())
+      .then(setAlunos);
 
-    // fetch("http://localhost:5000/api/turmas")
-    //   .then((res) => res.json())
-    //   .then(setTurmas);
-    setAlunos([])
-    setTurmas([])
+    fetch("http://localhost:5000/api/turmas")
+      .then((res) => res.json())
+      .then(setTurmas);
   }, []); // [] = roda só uma vez, ao montar o componente
 
   const alunosFiltrados = alunos
